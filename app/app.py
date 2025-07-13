368be4fbf18e913b96c1c0510cd81833dc545103
harshithagm <gmharshitha123@gmail.com>
2025-07-13 13:56:54 +0530

    changes in app

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return "Hello from Flask CI/CD Pipeline!"
+    return "Hello from Flask CI/CD Pipeline!"
 
-    if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=5000)
-            
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
